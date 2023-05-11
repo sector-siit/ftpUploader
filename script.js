e4ff7eba1b046a7f5680427ec9107d4fa81dc0f7
Pablo Ivan Lugo <androide1169836654@gmail.com>
2023-05-11 17:08:10 -0300

    fix

---
+++ script.js
@@ -0,0 +1,36 @@
+  const dotenv = require('dotenv')
+const FtpDeploy = require('ftp-deploy')
+const path = require('path')
+
+dotenv.config({ path: './.env' })
+
+async function main() {
+  try {
+    console.log(process.env)
+    // Replace "/out" with your build directory which contains all generated static files
+    const outDir = path.join(process.cwd(), process.env.BUILD_DIRECTORY || '/out')
+    await new FtpDeploy().deploy({
+      user: process.env.FTP_USER, // Your credentials
+      password: process.env.FTP_PASS, // Your credentials
+      host: process.env.FTP_HOST, // Your credentials
+      port: 21, // Your credentials
+
+      localRoot: outDir, // Location of build files in project
+      remoteRoot: process.env.REMOTE_DIRECTORY, // Upload location on remote, replace with subfolder on FTP-server if required
+
+      include: ['*', '**/*'], // Upload all files from build folder
+      exclude: [], // Exclude no files
+
+      deleteRemote: true, // Set to true if you want to delete ALL FILES in the remote root before uploading
+      forcePasv: true // Use passive mode
+    })
+
+    console.log('Succesfully deployed site')
+    return 0
+  } catch (e) {
+    console.error('An error occured during deployment:', e)
+    return 1
+  }
+}
+
+main().then((code) => process.exit(code))
