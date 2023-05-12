8b5754d89351902bf8b2efedd8653912ef856737
androide2374 <androide1169836654@gmail.com>
2023-05-12 20:24:05 -0300

    Create delete.sh

---
+++ delete.sh
@@ -0,0 +1,31 @@
+ftp ftp://siit:Rojo1212@172.16.0.74 <<EOF
+prompt
+cd api
+cd UsuarioUnico
+nlist . lista_archivos.txt
+bye
+EOF
+archivos=$(cat lista_archivos.txt | grep -v 'appsettings\|wwwroot\|Mail_template')
+for archivo in $archivos
+do
+echo $archivo
+ftp ftp://siit:Rojo1212@172.16.0.74 <<EOF
+prompt
+cd api
+cd UsuarioUnico
+pwd
+mdelete $archivo
+bye
+EOF
+done
+for archivo in $archivos
+do
+echo $archivo
+ftp ftp://siit:Rojo1212@172.16.0.74 <<EOF
+prompt
+cd api
+cd UsuarioUnico
+rm $archivo
+bye
+EOF
+done
